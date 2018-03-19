9dc95b866c80bb3eba1b852017706eb8002937c5
uh3ng <qyffxzh@163.com>
2018-03-19 18:21:25 +0800

    model meta class

--- learn/models.py
+++ learn/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
+    question_text = models.CharField('question content', max_length=200)
     pub_date = models.DateTimeField('date published')
 
     def __str__(self):
