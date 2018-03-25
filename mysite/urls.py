053184ca8def107a525c5c8a7d90e2847e766d13
uh3ng <qyffxzh@163.com>
2018-03-25 21:23:07 +0800

    framework

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from login import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('learn/', include('learn.urls')),
+    path('index/', views.index, name="index"),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
+    path('logout/', views.logout, name='logout'),
 ]
