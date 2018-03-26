c499ba0bef4c1dd91d072d2d66a1e0c298ef26c5
uh3ng <qyffxzh@163.com>
2018-03-26 11:25:06 +0800

    login form

--- login/views.py
+++ login/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
+from login import models
 # Create your views here.
 
 
@@ -8,6 +9,19 @@ def index(request):
 
 
 def login(request):
+    if request.method == "POST":
+        username = request.POST.get("username", None)
+        password = request.POST.get("password", None)
+        if username and password:
+            username = username.strip()
+            try:
+                user = models.User.objects.get(name=username)
+                if user.password == password:
+                    return redirect('/index/')
+                message = "密码不正确"
+            except:
+                message = "用户不存在"
+        return render(request, 'login/login.html', {'message': message})
     return render(request, 'login/login.html')
 
 
